0a9a19cd56c7ff4c30e6f8c16841435c4e1e3d2b
stephen <osstephen70@gmail.com>
2025-04-14 12:29:48 +0100

    implemented Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,26 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build the query
+    // 1. Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2. Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // const query =  Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // EXECUTE the query
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
+const app = require('./app');
 
 mongoose
   .connect(process.env.DATABASE, {
