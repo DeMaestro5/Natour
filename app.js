acdcaaf3a40a871afb244f5775097488bd692572
stephen <osstephen70@gmail.com>
2025-04-15 09:13:51 +0100

    Add requestTime to app.js middleware and validate tour name length and price discount in tourModel.js

--- app.js
+++ app.js
@@ -12,14 +12,21 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('Middleware running');
-//   next();
-// });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // Routes
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
