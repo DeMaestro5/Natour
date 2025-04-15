e2c6dbae6493cb0999eeedc452b2fe9f7eddeecf
stephen <osstephen70@gmail.com>
2025-04-15 19:05:41 +0100

    Add bcryptjs package for password hashing and authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
-
+const { signup } = authController;
 const { getAllUsers, createNewUser, getUserById, updateUser, deleteUser } =
   userController;
-
+router.route('/signup').post(signup);
 router.route('/').get(getAllUsers).post(createNewUser);
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
 
