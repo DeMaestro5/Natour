840ee5add1b736bb9d815070bf56e8d6b1afd38e
stephen <osstephen70@gmail.com>
2025-04-15 12:42:53 +0100

    Refactor errorController.js to handle errors in development and production environments, and added error handling for duplicate fields and validation errors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled exception! Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
@@ -13,10 +19,19 @@ mongoose
     console.log('DB connection successful');
   })
   .catch((err) => {
-    console.error('DB connection error:', err);
+    console.log('DB connection failed');
+    console.log(err);
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled rejection! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
