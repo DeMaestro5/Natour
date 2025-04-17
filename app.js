06772f808fac6ff5c2d3daeee5b8c195a4c48555
stephen <osstephen70@gmail.com>
2025-04-17 21:23:42 +0100

    Add security middleware for HTTP headers, data sanitization, and parameter pollution prevention

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const usersRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
-app.use(express.json());
+
 // Global middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -20,6 +28,26 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'price',
+      'difficulty',
+    ], // whitelist specific query parameters
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
