2f1169457dd3bdf96833a467e9861cea8ec72dc9
stephen <osstephen70@gmail.com>
2025-04-17 20:50:53 +0100

    Add rate limiting middleware for API requests

--- app.js
+++ app.js
@@ -4,23 +4,33 @@ const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(express.json());
-
+// Global middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Middleware to add request time
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // Routes
-
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
